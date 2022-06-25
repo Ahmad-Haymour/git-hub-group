24e2716801c95801064e2cf66f98394c3293cde7
minoolee <yaqoubiamina@gmail.com>
2022-06-25 19:44:33 +0200

    navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div><ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li></ul></div>
   )
 }
